a653fa8dd78d70cedddf43e5dcc28eb78e3cc343
Joshua Bolasa <bolasajoshua73@gmail.com>
2024-10-05 13:58:58 +0800

    updated contact design

--- src/Pages/help/Contact.jsx
+++ src/Pages/help/Contact.jsx
@@ -4,16 +4,16 @@ export default function Contact() {
     const data = useActionData();
 
     return (
-        <div className="contact">
+        <div className="contact bg-red-900">
             <h3>Contact Us</h3>
-            <Form method="post">
+            <Form method="post" className="flex items-center flex-row gap-2">
                 <label>
                     <span>Your email:</span>
                     <input type="email" name="email" required />
                 </label>
                 <label>
                     <span>Your message:</span>
-                    <textarea name="message" required></textarea>
+                    <textarea name="message" required className="resize-none"></textarea>
                 </label>
                 <button>Submit</button>
 
