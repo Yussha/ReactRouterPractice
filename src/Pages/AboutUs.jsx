123d57abc33ac701faf8e58458b493abd5061f21
Joshua Bolasa <bolasajoshua73@gmail.com>
2024-10-05 14:05:32 +0800

    update about us description

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -11,9 +11,20 @@ export default function About() {
     return (
         <div className="p-4">
             <h2>About us</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi eveniet earum. Dolorem, vel? Explicabo veniam, id, laudantium sunt amet pariatur unde porro architecto ims sequi maiores illo enim fugiat. Maxime, nam quam animi a, reiciendis atque similique voluptates aliquid, ratione enim sequi voluptas dignissimos exercitationem ducimus vero. Nobis facilis corporis, neque consequatur ipsum natus cum vero, eligendi deserunt optio nesciunt laudantium, in reprehenderit unde magnam. Possimus alias recusandae et soluta numquam vel.
-            </p>
+            <div className="flex flex-col gap-8">
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi eveniet earum. Dolorem, vel? Explicabo veniam, id, laudantium sunt amet pariatur unde porro architecto ims sequi maiores illo enim fugiat. Maxime, nam quam animi a, reiciendis atque similique voluptates aliquid, ratione enim sequi voluptas dignissimos exercitationem ducimus vero. Nobis facilis corporis, neque consequatur ipsum natus cum vero, eligendi deserunt optio nesciunt laudantium, in reprehenderit unde magnam. Possimus alias recusandae et soluta numquam vel.
+                </p>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi eveniet earum. Dolorem, vel? Explicabo veniam, id, laudantium sunt amet pariatur unde porro architecto ims sequi maiores illo enim fugiat. Maxime, nam quam animi a, reiciendis atque similique voluptates aliquid, ratione enim sequi voluptas dignissimos exercitationem ducimus vero. Nobis facilis corporis, neque consequatur ipsum natus cum vero, eligendi deserunt optio nesciunt laudantium, in reprehenderit unde magnam. Possimus alias recusandae et soluta numquam vel.
+                </p>
+
+                <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad excepturi eveniet earum. Dolorem, vel? Explicabo veniam, id, laudantium sunt amet pariatur unde porro architecto ims sequi maiores illo enim fugiat. Maxime, nam quam animi a, reiciendis atque similique voluptates aliquid, ratione enim sequi voluptas dignissimos exercitationem ducimus vero. Nobis facilis corporis, neque consequatur ipsum natus cum vero, eligendi deserunt optio nesciunt laudantium, in reprehenderit unde magnam. Possimus alias recusandae et soluta numquam vel.
+                </p>
+            </div>
+
 
             <button onClick={() => setUser(null)} className="bg-orange-400 p-2">Logout</button>
         </div>
